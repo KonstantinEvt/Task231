b7c642b281c3128b607336b83be60f0c2e08abfb
Konstantin Evtushenko <K-S-Y@yandex.ru>
2024-06-28 15:14:29 +0300

    branch 2

---
+++ src/main/java/crud/controller/TestClass2.java
@@ -0,0 +1,11 @@
+package crud.controller;
+
+public class TestClass2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+
+    }
+}
